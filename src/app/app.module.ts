1b9804a307590da4e78c1a566f96f0f92ba8c0ec
陈旭 <chen.xu8@zte.com.cn>
2019-05-11 23:58:49 +0800

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {JigsawModule} from '@rdkmaster/jigsaw';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {AppComponent} from './app.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule, FormsModule, BrowserAnimationsModule,
+        JigsawModule, TranslateModule.forRoot()
+    ],
+    providers: [TranslateService],
+    bootstrap: [AppComponent],
+    entryComponents: []
 })
-export class AppModule { }
+export class AppModule {
+    constructor(translateService: TranslateService) {
+        translateService.setTranslation('zh', {
+            'get-started': '马上开始',
+            'give-star': '给 Jigsaw 点个星星'
+        });
+        translateService.setTranslation('en', {
+            'get-started': 'Get started',
+            'give-star': 'Give us a star on Github.com'
+        });
+
+        const lang: string = translateService.getBrowserLang();
+        translateService.setDefaultLang(lang);
+        translateService.use(lang);
+    }
+}
